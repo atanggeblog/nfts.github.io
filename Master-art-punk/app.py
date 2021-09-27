3ddd58b94eed88dd9550625810574c6dddd8433f
philipjhc <2859322817@qq.com>
2021-09-27 23:09:18 +0800

    Master-art-punk
    Make your master artistic punk avatar through machine learning world famous paintings.

--- Master-art-punk/app.py
+++ Master-art-punk/app.py
@@ -5,11 +5,11 @@
 @Date   ：2021/9/22 17:04
 @Desc   ：主接口
 =================================================="""
-from colors import merges, generate
+from colors import ColorMultiImage
 from imageData.subject import canvas, cattle
-
 if __name__ == '__main__':
+    generate_color = ColorMultiImage()
     stickers = [canvas, cattle]  # 设置模组已经内置mouse和cattle
     for amount in range(0, 10):  # 设置生成数量
-        pixel = merges(stickers)
-        generate(pixel, 'cattle-output/cattle' + str(amount))
+        pixel = generate_color.merges(stickers)
+        generate_color.generate(pixel, 'cattle-output/cattle' + str(amount))
--- Master-art-punk/settings.py
+++ Master-art-punk/settings.py
@@ -5,6 +5,9 @@
 @Date   ：2021/9/24 19:12
 @Desc   ： 基础配置文件
 =================================================="""
-
 color_data_path = './Master-art-punk/Master-art-punk/Master-art-punk/data/Monet'  # 这里填入你的训练集文件夹路径例如：\\你的电脑路径\\data\\monet\
 color_distance_filepath = 'data/color_distance.csv'
+k = 30  # K-Means算法分成K类
+init_method = 'random'
+random_state = 88
+box_number = 8 # 艺术家风格分箱颜色数
